27ddcfd72a15fa6cef5853e1fcdf686b4c677e2e
Dylan <dylanlancekemboi@gmail.com>
2024-01-23 11:33:13 +0300

    Completed Lab

--- server/models.py
+++ server/models.py
@@ -1,11 +1,11 @@
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+
+from database import db
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
-db = SQLAlchemy(metadata=metadata)
 
 class Owner(db.Model):
     __tablename__ = 'owners'
